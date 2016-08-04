701511cbc7c874dc8bdb7cc83091e09249837a53
Siyuan Zhao <siyuan.vince.zhao@gmail.com>
2016-08-03 23:49:50 -0400

    runnable version

--- wide_n_deep_tutorial.py
+++ wide_n_deep_tutorial.py
@@ -21,6 +21,7 @@ import tempfile
 
 import pandas as pd
 import tensorflow as tf
+import numpy as np
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -28,7 +29,7 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string("model_dir", "", "Base directory for output models.")
 flags.DEFINE_string("model_type", "wide_n_deep",
                     "Valid model types: {'wide', 'deep', 'wide_n_deep'}.")
-flags.DEFINE_integer("train_steps", 200, "Number of training steps.")
+flags.DEFINE_integer("train_steps", 400, "Number of training steps.")
 flags.DEFINE_string(
     "train_data",
     "",
@@ -38,33 +39,33 @@ flags.DEFINE_string(
     "",
     "Path to the test data.")
 
-COLUMNS = ["Semana", "Agencia_ID", "Cancal_ID", "Ruta_SAK", "Cliente_ID",
+COLUMNS = ["Semana", "Agencia_ID", "Canal_ID", "Ruta_SAK", "Cliente_ID",
            "Producto_ID", "Demanda_uni_equil"]
 LABEL_COLUMN = "Demanda_uni_equil"
-CATEGORICAL_COLUMNS = ["Agencia_ID", "Cancal_ID", "Ruta_SAK", "Cliente_ID", "Producto_ID"]
+CATEGORICAL_COLUMNS = ["Agencia_ID", "Canal_ID", "Ruta_SAK", "Cliente_ID", "Producto_ID"]
 CONTINUOUS_COLUMNS = ["Semana"]
 
 def build_estimator(model_dir):
 
     agencia_id = tf.contrib.layers.sparse_column_with_hash_bucket("Agencia_ID", hash_bucket_size=10)
-    cancal_id = tf.contrib.layers.sparse_column_with_hash_bucket("Cancal_ID", hash_bucket_size=10)
+    cancal_id = tf.contrib.layers.sparse_column_with_hash_bucket("Canal_ID", hash_bucket_size=10)
     ruta_sak = tf.contrib.layers.sparse_column_with_hash_bucket("Ruta_SAK", hash_bucket_size=10)
-    cliente_id = tf.contrib.layers.sparse_column_with_hash_bucket("Cliente_ID", hash_bucket_size=1000)
+    cliente_id = tf.contrib.layers.sparse_column_with_hash_bucket("Cliente_ID", hash_bucket_size=4000)
     producto_id = tf.contrib.layers.sparse_column_with_hash_bucket("Producto_ID", hash_bucket_size=1000)
 
     # Continuous base columns.
-    semana = tf.contrib.layers.real_valued_column("semana")
+    semana = tf.contrib.layers.real_valued_column("Semana")
     # Wide columns and deep columns.
     deep_columns = [
-      tf.contrib.layers.embedding_column(agencia_id, dimension=8),
-      tf.contrib.layers.embedding_column(cancal_id, dimension=8),
-      tf.contrib.layers.embedding_column(ruta_sak, dimension=8),
-      tf.contrib.layers.embedding_column(cliente_id, dimension=64),
-      tf.contrib.layers.embedding_column(producto_id, dimension=64),
+      tf.contrib.layers.embedding_column(agencia_id, dimension=4),
+      tf.contrib.layers.embedding_column(cancal_id, dimension=4),
+      tf.contrib.layers.embedding_column(ruta_sak, dimension=4),
+      tf.contrib.layers.embedding_column(cliente_id, dimension=32),
+      tf.contrib.layers.embedding_column(producto_id, dimension=16),
       semana,
     ]
 
-    m = tf.contrib.learn.DNNClassifier(model_dir=model_dir, feature_columns=deep_columns,hidden_units=[200, 100])
+    m = tf.contrib.learn.DNNRegressor(model_dir=model_dir, feature_columns=deep_columns,hidden_units=[200, 100], optimizer=tf.train.AdamOptimizer(0.1))
     return m
 
 
@@ -92,16 +93,23 @@ def input_fn(df):
 def train_and_eval():
     """Train and evaluate the model."""
 
-    df_train = pd.read_csv('train.csv', names=COLUMNS, skipinitialspace=True)
+    df_train = pd.read_csv('bimbo_train.csv', delimiter=',',
+                           dtype={'Semana':np.int32, 'Agencia_ID': str, 'Canal_ID': str,
+                                  'Ruta_SAK': str, 'Cliente_ID': object, 'Producto_ID': str, 'Demanda_uni_equil': np.int32})
 
-    df_eval = df_train[1000:1010]
+    df_eval = pd.read_csv('bimbo_val.csv', delimiter=',',
+                          dtype={'Semana':np.int32, 'Agencia_ID': str, 'Canal_ID': str,
+                                 'Ruta_SAK': str, 'Cliente_ID': object, 'Producto_ID': str, 'Demanda_uni_equil': np.int32})
+
     model_dir = tempfile.mkdtemp() if not FLAGS.model_dir else FLAGS.model_dir
     print("model directory = %s" % model_dir)
 
     m = build_estimator(model_dir)
     m.fit(input_fn=lambda: input_fn(df_train), steps=FLAGS.train_steps)
     preds = m.predict(input_fn=lambda: input_fn(df_eval))
-    print (preds)
+    rmlse = np.sqrt(np.mean(np.square(np.log1p(preds) - np.log1p(df_eval['Demanda_uni_equil']))))
+    # print (preds)
+    print (rmlse)
     # results = m.evaluate(input_fn=lambda: input_fn(df_test), steps=1)
     # for key in sorted(results):
     #    print("%s: %s" % (key, results[key]))
